365dff9e4294ea876971d82f470ee209d21a7470
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2024-10-23 23:10:12 +0200

    Add use summaries hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import {useSummaries} from "@/hooks/useSummaries";
 
 export type Summary = {
     title: string;
     summary: string;
+    url: string;
+    date: string;
 }
 
 export type Summaries = {
@@ -11,76 +15,31 @@ export type Summaries = {
 }
 
 export default function Home() {
-    const summaries: Summaries = {
-        "summaries": [
-            {
-                "title": "Tyrkias regjering: Terrorangrep mot flybase like utenfor Ankara",
-                "summary": "Tyrkiske innenriksministeren Ali Yerlikaya kaller det et terrorangrep på X. Han sier at flere er skadd eller døde."
-            },
-            {
-                "title": "Roma venter turistrekord",
-                "summary": "Roma skal gjøres i stand til den katolske kirkens store jubelår i 2025. Julaften må det meste være klart, for da skal pave Frans åpne den hellige døren inn til Peterskirken. Kritikerne mener det trengs et mirakel for å rekke det."
-            }
-        ]
-    }
+    const summaries = useSummaries()
 
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <div>
-                    Summaries:
+                <h1>LITT NYTT</h1>
+                <h3>Nyheter, kort fortalt</h3>
+                <ul className={styles.list}>
                     {summaries && summaries.summaries?.length != 0 &&
                         summaries.summaries.map(summary =>
                             (
-                                <div key={summary.title}>
-                                    <h1>{summary.title}</h1>
-                                    <p>{summary.summary}</p>
-                                    <br></br>
-                                </div>
+                                <a
+                                    href={summary.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    key={summary.title}
+                                >
+                                    <li className={styles.listitem} key={summary.title}>
+                                        <h1>{summary.title}</h1>
+                                        <p>{summary.summary}</p>
+                                    </li>
+                                </a>
                             )
                         )}
-                </div>
-
-                <Image
-                    className={styles.logo}
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol>
-                    <li>
-                        Get started by editing <code>app/page.tsx</code>.
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
-
-                <div className={styles.ctas}>
-                    <a
-                        className={styles.primary}
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className={styles.logo}
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={styles.secondary}
-                    >
-                        Read our docs
-                    </a>
-                </div>
+                </ul>
             </main>
             <footer className={styles.footer}>
                 <div>
