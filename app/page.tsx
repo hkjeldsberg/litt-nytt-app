b9d5ea689bbc8c3ae7e7a6be562803307fced4f4
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2024-10-23 21:57:44 +0200

    Add icons

--- app/page.tsx
+++ app/page.tsx
@@ -83,48 +83,37 @@ export default function Home() {
                 </div>
             </main>
             <footer className={styles.footer}>
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
+                <div>
+                    <div>Laget av Henrik Aasen Kjeldsberg</div>
+                    <div>
+                        <a
+                            href="https://www.linkedin.com/in/hkjeldsberg/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                aria-hidden
+                                src="/linkedin.svg"
+                                alt="Linkedin icon"
+                                width={32}
+                                height={32}
+                            />
+                        </a>
+                        <a
+                            href="https://github.com/HKjeldsberg"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                aria-hidden
+                                src="/github.svg"
+                                alt="GitHub icon"
+                                width={32}
+                                height={32}
+                            />
+                        </a>
+                    </div>
+                </div>
             </footer>
         </div>
     );
